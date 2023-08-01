8b50006067e89bb1b774b8ce0afd94437d305a7c
moezzouari <moez@octoplus.tech>
2023-08-01 13:55:01 +0100

    Add Navscroll and path in App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ArticleApp from './Components/articles/ArticleApp';
+import CategorieApp from './Components/categories/CategorieApp';
+import ScategorieApp from './Components/scategories/ScategorieApp';
+import Navscroll from './Components/Navscroll';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navscroll />
+      <Routes>
+        <Route path="/articles" element={<ArticleApp />}></Route>
+        <Route path="/categories" element={<CategorieApp />}></Route>
+        <Route path="/scategories" element={<ScategorieApp />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
